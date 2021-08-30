d16dca0bda0e8da613701c05a591c22a2973e75c
Agent 0005 <89727598+Agent0005@users.noreply.github.com>
2021-08-30 10:53:31 +0545

    Update index.ts

--- src/hooks/useArticles/index.ts
+++ src/hooks/useArticles/index.ts
@@ -7,7 +7,7 @@ export function useArticles() {
   const [articles, setArticles] = useState<Articles[]>([]);
 
   useEffect(() => {
-    api.get(`top-headlines?sources=techcrunch&apiKey=cf370461f1a249c38cc72997821354d1`).then(response => {
+    api.get(`posts`).then(response => {
       setArticles(response.data);
     });
   }, []);
