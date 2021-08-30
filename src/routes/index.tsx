ba6ea090dbdd2e2ab550b06af0f25a274f588395
Agent 0005 <89727598+Agent0005@users.noreply.github.com>
2021-08-30 07:56:35 +0545

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
+import { Blog } from '../containers/Blog';
 import { Profile } from '../containers/Profile';
 import { Repositories } from '../containers/Repositories';
 
@@ -11,7 +11,7 @@ export function Router() {
       <Routes>
         <Route path="/" element={<Profile />} />
         <Route path="/repositories" element={<Repositories />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="https://google.com" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
