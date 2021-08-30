4060f955c2cde48d34aef99a047b550c1dff2fd5
Agent 0005 <89727598+Agent0005@users.noreply.github.com>
2021-08-30 07:08:47 +0545

    Update index.ts

--- src/hooks/useArticles/index.ts
+++ src/hooks/useArticles/index.ts
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Articles } from '../../shared';
-import { USERNAME_KEY } from '../../constants/params';
+
 import { api } from '../../services/devto.api';
 
 export function useArticles() {
   const [articles, setArticles] = useState<Articles[]>([]);
 
   useEffect(() => {
-    api.get(`articles?username=${USERNAME_KEY}`).then(response => {
+    api.get(`feed`).then(response => {
       setArticles(response.data);
     });
   }, []);
